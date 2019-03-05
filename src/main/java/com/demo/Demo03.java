b24e1a8740b818ed37e68f987591c7baaa94e259
zhangsan <zhangsan@itcast.cn>
2019-03-05 17:16:18 +0800

    新增demo03

---
+++ src/main/java/com/demo/Demo03.java
@@ -0,0 +1,9 @@
+package com.demo;
+
+public class Demo03 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
